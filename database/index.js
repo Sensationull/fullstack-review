740f9a98e7f3b0aa2f62630670df5da5105e9ee6
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-10-19 19:56:54 -0700

    added schema and database connectivity

--- database/index.js
+++ database/index.js
@@ -1,16 +1,47 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher',{ useNewUrlParser: true });
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+
+mongoose.connection.once('open', function (){
+ console.log('connection success')
+}).on('error', function(error){
+ console.log('Connection error: ',error)
+})
+
+let repoSchema = new mongoose.Schema({
+  iD: Number, 
+  userID: Number,
+  userName: String,
+  repoName: String,
+  forkCount: Number,
+  uRL: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+// I don't think I need to save the find function because it's a mongo native function?
+
+let save = (model) => {
+  model.save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  console.log(model, ' was saved to MongoDB'); // I'm telling it to log, but I'm not sure if I need to.
 }
+
+var test = new Repo({
+  iD: 1,
+  userID: 123,
+  userName: 'test',
+  repoName: 'repotest',
+  forkCount: 13,
+  uRL: 'abc123.com'
+});
+
+console.log('this is a ', test);
+test.save(test);
 
 module.exports.save = save;
